9807afbe6a3ec5ec2c352cdfa8043bd94e88c36d
João Victor <jvoliveira2005@gmail.com>
2023-06-08 19:44:23 -0300

    Customizando lista de cursos
    Parei no video 08

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { CoursesModule } from './components/courses.module';
+
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    CoursesModule,
-    AppRoutingModule
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
